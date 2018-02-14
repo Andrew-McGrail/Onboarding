3adf8856e2ffeee9547c5458bd83e8e3b65a81d8
unknown <andrew.mcgrail@GSOD-AMCGRAIL.orasi.com>
2018-02-14 14:06:15 -0500

    Partially done with 8

---
+++ src/test/java/excercises/Exercise10.java
@@ -0,0 +1,18 @@
+package excercises;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Exercise10 {
+		
+		@Test
+		public void testExercise10() {
+			
+			WebDriver driver = new ChromeDriver();
+
+			
+			driver.close();
+		}
+
+}
---
+++ src/test/java/excercises/Exercise9.java
@@ -0,0 +1,17 @@
+package excercises;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Exercise9 {
+	
+	@Test
+	public void testExercise9() {
+		
+		WebDriver driver = new ChromeDriver();
+
+		
+		driver.close();
+	}
+}
